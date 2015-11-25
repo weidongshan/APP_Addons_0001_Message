77ea8835394c5845e22a00d8119a3d439d884b81
weidongshan <weidongshan@qq.com>
2015-11-25 15:02:19 +0800

    v1, base version, support Button

---
+++ app/src/main/java/com/thisway/app_addons_0001_message/MainActivity.java
@@ -0,0 +1,54 @@
+package com.thisway.app_addons_0001_message;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.view.View;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mButton;
+    private final String TAG="MessageTest";
+    private int ButtonCount = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mButton = (Button)findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                Log.d(TAG, "Send Message "+ ButtonCount);
+                ButtonCount++;
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
